b64a8a623a61b7fac2b65bea5d762d61fc8cf9a8
brahmxx <bhm.x@live.com>
2025-10-01 00:48:17 +0100

    Added reviews api, Added tabs to products page that contains details, review, faq

--- src/components/FeedbackCard.js
+++ src/components/FeedbackCard.js
@@ -4,7 +4,7 @@ import { CheckCircleFilled } from "@ant-design/icons";
 const FeedbackCard = ({ name, location, comment }) => {
   return (
     <>
-      <div className="flex flex-col flex-shrink-0 border-1 border-gray-300/40 w-[98%] p-[25px]  h-[190px] lg:h-[260px] xl:h-[280px] lg:gap-5 rounded-xl hover:shadow-md transition-shadow duration-300 cursor-pointer my-4">
+      <div className="flex flex-col flex-shrink-0 border-1 border-gray-300/40 w-[98%] p-[25px]  h-[190px] lg:h-[260px] xl:h-[280px] lg:gap-5 rounded-[26px] hover:shadow-md transition-shadow duration-300 cursor-pointer my-4">
         <div className="flex flex-row justify-between items-center">
           <div className="flex flex-col">
             <span className="flex items-center gap-2 text-[16px] font-bold  lg:text-[18px]">
