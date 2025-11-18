8e58e0ebf09c0995caf39c97eb657722230554c3
brahimxx <bhm.x@live.com>
2025-11-18 17:03:08 +0100

    Now db is connected through a string

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,5 @@
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT || 3306,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
 export default pool;
